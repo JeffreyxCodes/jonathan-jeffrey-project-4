29dcf506262035101401a7acb57dc09116500539
Jonathan McWilliam <jonathanmcwilliam@gmail.com>
2019-05-22 21:35:55 -0400

    proposed data structure change

--- scripts/jonathanSongList.js
+++ scripts/jonathanSongList.js
@@ -1,6 +1,80 @@
-const x = {
-   artist: "",
-   track: "",
-   lyric: "",
-   convert: ["word1", "word2", ...]
-};
+const songList = [
+  {
+    artist: "Bruno Mars",
+    track: "Uptown Funk",
+    convert: [
+      "Ice",
+      "Gold",
+      "Masterpieces",
+      "City",
+      "Chucks",
+      "Kiss",
+      "Hot",
+      "Police",
+      "Fireman"
+    ],
+    lyrics: ` This hit, that ice cold
+            Michelle Pfeiffer, that white gold
+            This one for them hood girls
+            Them good girls straight masterpieces
+            Stylin', wilin', livin' it up in the city
+            Got Chucks on with Saint Laurent
+            Got kiss myself, I'm so pretty
+            I'm too hot (hot damn)
+            Called a police and a fireman
+            I'm too hot (hot damn)
+            Make a dragon wanna retire man
+            I'm too hot (hot damn)
+            Say my name you know who I am
+            I'm too hot (hot damn)
+            Am I bad 'bout that money, break it down
+            Girls hit your hallelujah (whoo)
+            Girls hit your hallelujah (whoo)
+            Girls hit your hallelujah (whoo)
+            'Cause uptown funk gon' give it to you
+            'Cause uptown funk gon' give it to you
+            'Cause uptown funk gon' give it to you
+            Saturday night and we in the spot
+            Don't believe me just watch (come on)
+            Don't believe…`
+  },
+  {
+    artist: "Bruno Mars",
+    track: "Uptown Funk",
+    convert: [
+      "Ice",
+      "Gold",
+      "Masterpieces",
+      "City",
+      "Chucks",
+      "Kiss",
+      "Hot",
+      "Police",
+      "Fireman"
+    ],
+    lyrics: ` This hit, that ice cold
+            Michelle Pfeiffer, that white gold
+            This one for them hood girls
+            Them good girls straight masterpieces
+            Stylin', wilin', livin' it up in the city
+            Got Chucks on with Saint Laurent
+            Got kiss myself, I'm so pretty
+            I'm too hot (hot damn)
+            Called a police and a fireman
+            I'm too hot (hot damn)
+            Make a dragon wanna retire man
+            I'm too hot (hot damn)
+            Say my name you know who I am
+            I'm too hot (hot damn)
+            Am I bad 'bout that money, break it down
+            Girls hit your hallelujah (whoo)
+            Girls hit your hallelujah (whoo)
+            Girls hit your hallelujah (whoo)
+            'Cause uptown funk gon' give it to you
+            'Cause uptown funk gon' give it to you
+            'Cause uptown funk gon' give it to you
+            Saturday night and we in the spot
+            Don't believe me just watch (come on)
+            Don't believe…`
+  }
+];
