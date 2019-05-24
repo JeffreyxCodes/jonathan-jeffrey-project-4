4faff3a285ea0f38ac6bbc4f1b5d44e21c2965fe
jeffreyxdc <jeffreyxdc@gmail.com>
2019-05-24 14:17:51 -0400

    added hints logic

--- scripts/script.js
+++ scripts/script.js
@@ -7,12 +7,14 @@ app.$total = $(`.totalScore span`);
 app.$current = $(`.currentScore span`);
 
 app.totalScore = 0;
-app.currentScore = 5;
+app.currentScore = 4;
+app.hintIndex = 0;
 
 app.quizList = [];
 app.currentSongIndex = undefined;
 app.currentSong = undefined;
 
+// set the list of songs for the game
 app.setQuizList = function () {
     const index = Object.keys(songArray);
     const length = index.length;
@@ -26,6 +28,16 @@ app.setQuizList = function () {
     app.currentSong = songArray[app.currentSongIndex];
 };
 
+// populate the drop down menu with appropriate options
+app.populateDropDown = function () {
+    const options = songArray.map(song => {
+        return `<option value="${song.track}">"${song.track}" by ${song.artist}</option>>`;
+    });
+    // put the drop down options onto the DOM
+    app.$select.html(options);
+};
+
+// get a promise given a query
 app.getImagePromise = function (q) {
     return $.ajax({
         url: `https://pixabay.com/api/`,
@@ -39,21 +51,7 @@ app.getImagePromise = function (q) {
     });
 };
 
-// app.addScore = function() {
-//     if(app.quizList.length > 0 && app.quizList[app.quizList.length-1]){
-
-//     }
-// };
-
-app.populateDropDown = function () {
-    const options = songArray.map(song => {
-        return `<option value="${song.track}">"${song.track}" by ${
-            song.artist
-            }</option>>`;
-    });
-    app.$select.html(options);
-};
-
+// get and put the images related to a song
 app.getImages = function () {
     const imgPromises = [];
 
@@ -64,9 +62,12 @@ app.getImages = function () {
     $.when(...imgPromises)
         .then((...images) => {
             images = images.map(img => {
-                return `<img class="cover" src=${img[0].hits[0].webformatURL} alt="${img[0].hits[0].tags}">`;
+                return `<div class="imgContainer">
+                            <h2 class="hint">${img[0].hits[0].tags}</h2>
+                            <img class="cover" src=${img[0].hits[0].webformatURL} alt="${img[0].hits[0].tags}">
+                        </div>`;
             });
-
+            // put the images into the DOM
             app.$gallery.html(images);
         })
         .fail(error => {
@@ -74,35 +75,45 @@ app.getImages = function () {
         });
 };
 
+// initialize the submit button
 app.initSubmit = function () {
     app.$form.on("submit", e => {
         e.preventDefault();
 
-        if (app.quizList.length > 0) {
-            if (app.currentSong.track === app.$select.val()) {
+        if (app.quizList.length > 0) { // if there are still songs on the list
+            if (app.currentSong.track === app.$select.val()) { // when user is correct
+                // update the total score and current score
                 app.totalScore += app.currentScore;
                 app.$total.html(app.totalScore);
                 app.$current.html(4);
 
+                // update the song list and current song
                 app.quizList.pop();
                 app.currentSongIndex--;
                 app.currentSong = songArray[app.currentSongIndex];
 
+                // populate the dom with the elements for the next song
                 app.getImages();
                 app.populateDropDown();   
-            } else if (false) {
+            } else if (app.currentScore > 0) {
+                // update the current score
+                app.currentScore--;
+                app.$current.html(app.currentScore);
 
+                // app.$gallery
             }
         }
     });
 };
 
+// initialize the start button
 app.initStart = function () {
     $("button").on("click", () => {
         $(".instructionModal").fadeOut();
     });
 };
 
+// init
 app.init = function () {
     app.initStart();
 
